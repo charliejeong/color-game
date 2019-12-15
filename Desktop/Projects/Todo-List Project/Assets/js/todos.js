64e40dc632480fc731ab3e0c7298396e076f314e
Charlie <jeehoon.jeong.711@gmail.com>
2019-12-15 10:35:47 -0600

    used on method instead of click method

--- Desktop/Projects/Todo-List Project/Assets/js/todos.js
+++ Desktop/Projects/Todo-List Project/Assets/js/todos.js
@@ -1,13 +1,23 @@
 //check off specific todos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	e.stopPropagation();
+});
+
+//add new todo
+$("input[type='text']").keypress(function(e){
+	if (e.which === 13) {
+		var todoText = $(this).val();
+		$(this).val("");
+	//create new li and add to ul
+	$("ul").append("<li><span>x</span> " + todoText + "</li>");
+	}
 });
